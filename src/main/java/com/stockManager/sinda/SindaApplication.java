d6a683198349c4cb37b0af265c7072327281a27f
sinda-678 <sindajordan@gmail.com>
2022-04-24 15:10:04 +0200

    initiation de mon projet

---
+++ src/main/java/com/stockManager/sinda/SindaApplication.java
@@ -0,0 +1,13 @@
+package com.stockManager.sinda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SindaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SindaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockManager/sinda/SindaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockManager.sinda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SindaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
